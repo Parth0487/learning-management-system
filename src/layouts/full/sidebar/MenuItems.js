c17cf7ecb3acef5066b20ec165a9776714d40c31
Parth Bhavsar <bhavsar.parth@neviton.com>
2024-04-27 23:54:22 +0530

    Initial commit

---
+++ src/layouts/full/sidebar/MenuItems.js
@@ -0,0 +1,56 @@
+import {
+  IconAperture, IconCopy, IconLayoutDashboard, IconLogin, IconMoodHappy, IconTypography, IconUserPlus
+} from '@tabler/icons';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+
+  {
+    id: uniqueId(),
+    title: 'Dashboard',
+    icon: IconLayoutDashboard,
+    href: '/dashboard',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Typography',
+    icon: IconTypography,
+    href: '/ui/typography',
+  },
+  {
+    id: uniqueId(),
+    title: 'Shadow',
+    icon: IconCopy,
+    href: '/ui/shadow',
+  },
+  
+  {
+    id: uniqueId(),
+    title: 'Login',
+    icon: IconLogin,
+    href: '/auth/login',
+  },
+  {
+    id: uniqueId(),
+    title: 'Register',
+    icon: IconUserPlus,
+    href: '/register',
+  },
+  
+  {
+    id: uniqueId(),
+    title: 'Icons',
+    icon: IconMoodHappy,
+    href: '/icons',
+  },
+  {
+    id: uniqueId(),
+    title: 'Sample Page',
+    icon: IconAperture,
+    href: '/sample-page',
+  },
+];
+
+export default Menuitems;
